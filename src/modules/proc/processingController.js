5f838343f218033d33ca04a373df57ad8633e53c
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-07-09 11:48:55 -0500

    chore: limpiar repositorio a cero

--- src/modules/proc/processingController.js
+++
@@ -1,9 +0,0 @@
-function handleProcessImportedVideo() {
-  return Promise.resolve({ ok: false, message: 'Pendiente.' });
-}
-
-function handleGetMediaProcessingDiagnostic() {
-  return { ok: true, module: 'proc' };
-}
-
-module.exports = { handleProcessImportedVideo, handleGetMediaProcessingDiagnostic };
