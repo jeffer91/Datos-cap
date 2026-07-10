0895fc80abf976b0eebed673e7afdc9311227001
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-07-10 02:35:12 -0500

    Etapa 9: retirar diagnóstico temporal del Plan General

--- src/diagnostics/plan-general-capacitacion.debug.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const { assertProcessor } = require("../core/processor.registry");
-const { createSyntheticGeneralPlan } = require("./plan-general-capacitacion.parser.test");
-
-const processor = assertProcessor("plan-general-capacitacion");
-const result = processor.parseDocuments([createSyntheticGeneralPlan()]);
-console.log(JSON.stringify(result, null, 2));
