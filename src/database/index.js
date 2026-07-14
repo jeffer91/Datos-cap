38b683979389bf6c5f10257144d0617981b9d820
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-07-14 15:51:20 -0500

    Base: exponer servicio de consultas

--- src/database/index.js
+++ src/database/index.js
@@ -2,10 +2,12 @@
 Nombre completo: index.js
 Ruta o ubicación: /src/database/index.js
 Función o funciones:
-- Exponer el motor y el servicio de persistencia de la base local.
+- Exponer motor, persistencia y consultas de la base local.
 ========================================================= */
 "use strict";
+
 module.exports = {
   ...require("./local-database"),
-  ...require("./persistence.service")
+  ...require("./persistence.service"),
+  ...require("./query.service")
 };
