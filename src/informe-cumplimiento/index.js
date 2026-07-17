bbea402528648c9634c5e29faac05099639d3efb
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-07-16 22:44:01 -0500

    Exponer servicios ampliados del informe global

--- src/informe-cumplimiento/index.js
+++ src/informe-cumplimiento/index.js
@@ -1,4 +1,12 @@
 "use strict";
+
 const { createComplianceReportService, ComplianceReportService } = require("./informe-cumplimiento.service");
 const { buildGlobalReport, buildCoverage, calculateMetrics } = require("./informe-cumplimiento.engine");
-module.exports = { createComplianceReportService, ComplianceReportService, buildGlobalReport, buildCoverage, calculateMetrics };
+
+module.exports = {
+  createComplianceReportService,
+  ComplianceReportService,
+  buildGlobalReport,
+  buildCoverage,
+  calculateMetrics
+};
