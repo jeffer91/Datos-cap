78ae8fca64be67332a985417f75abb9f5e0126ee
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-08-04 15:02:34 -0500

    Reiniciar aplicación desde cero

--- src/informe-cumplimiento/guias/guia-versiones.service.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-class GuideVersionsService {
-  constructor(repository) { this.repository = repository; }
-  list(guideId) { return this.repository.versions(guideId); }
-}
-
-module.exports = { GuideVersionsService };
