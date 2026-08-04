7face0aac1899c6588e97f400ee0e265b1e39405
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-08-04 16:30:19 -0500

    Exponer API segura para acuerdos

--- preload.js
+++ preload.js
@@ -2,6 +2,13 @@
 
 const { contextBridge, ipcRenderer } = require("electron");
 
+function subscribe(channel, callback) {
+  if (typeof callback !== "function") return () => {};
+  const listener = (_event, payload) => callback(payload);
+  ipcRenderer.on(channel, listener);
+  return () => ipcRenderer.removeListener(channel, listener);
+}
+
 contextBridge.exposeInMainWorld("plansAPI", {
   selectFiles: () => ipcRenderer.invoke("plans:select-files"),
   selectFolder: () => ipcRenderer.invoke("plans:select-folder"),
@@ -13,10 +20,18 @@ contextBridge.exposeInMainWorld("plansAPI", {
   openFile: (filePath) => ipcRenderer.invoke("plans:open-file", filePath),
   clear: () => ipcRenderer.invoke("plans:clear"),
   getAppInfo: () => ipcRenderer.invoke("app:info"),
-  onProgress: (callback) => {
-    if (typeof callback !== "function") return () => {};
-    const listener = (_event, payload) => callback(payload);
-    ipcRenderer.on("plans:progress", listener);
-    return () => ipcRenderer.removeListener("plans:progress", listener);
-  }
+  onProgress: (callback) => subscribe("plans:progress", callback)
+});
+
+contextBridge.exposeInMainWorld("agreementsAPI", {
+  selectFiles: () => ipcRenderer.invoke("agreements:select-files"),
+  selectFolder: () => ipcRenderer.invoke("agreements:select-folder"),
+  process: (filePaths) => ipcRenderer.invoke("agreements:process", { filePaths }),
+  list: () => ipcRenderer.invoke("agreements:list"),
+  update: (record) => ipcRenderer.invoke("agreements:update", record),
+  export: (format) => ipcRenderer.invoke("agreements:export", { format }),
+  openFile: (filePath) => ipcRenderer.invoke("agreements:open-file", filePath),
+  clear: () => ipcRenderer.invoke("agreements:clear"),
+  getAppInfo: () => ipcRenderer.invoke("app:info"),
+  onProgress: (callback) => subscribe("plans:progress", callback)
 });
